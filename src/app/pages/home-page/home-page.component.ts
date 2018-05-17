c85af5374666bdb396797ee6a6adc17fdb143381
GeorgeCoupland <georgemcoupland@gmail.com>
2018-05-17 12:25:13 +0200

    fix styling, make homepage responsive´

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  loading = true;
+  anon: boolean;
+  user: any;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.userChange$.subscribe((user) => {
+      this.loading = false;
+      this.user = user;
+      this.anon = !user;
+      console.log(this.loading, this.user, this.anon)
+    });
+
   }
 
 }
